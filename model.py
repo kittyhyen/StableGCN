4170e4f0d751b76425ea39f5b4fc7751696ced06
kittyhyen <38484494+kittyhyen@users.noreply.github.com>
2020-12-16 04:02:32 +0900

    Add files via upload

---
+++ model.py
@@ -0,0 +1,163 @@
+import torch.nn as nn
+import torch
+import math
+import numpy as np
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+
+class GraphConvolution(nn.Module):
+
+    def __init__(self, in_features, out_features, residual=False, variant=False):
+        super(GraphConvolution, self).__init__() 
+        self.variant = variant
+        if self.variant:
+            self.in_features = 2*in_features 
+        else:
+            self.in_features = in_features
+
+        self.out_features = out_features
+        self.residual = residual
+        self.weight = Parameter(torch.FloatTensor(self.in_features,self.out_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.out_features)
+        self.weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj , h0 , lamda, alpha, l):
+        theta = math.log(lamda/l+1)
+        hi = torch.spmm(adj, input)
+        if self.variant:
+            support = torch.cat([hi,h0],1)
+            r = (1-alpha)*hi+alpha*h0
+        else:
+            support = (1-alpha)*hi+alpha*h0
+            r = support
+        output = theta*torch.mm(support, self.weight)+(1-theta)*r
+        if self.residual:
+            output = output+input
+        return output
+
+class GCNII(nn.Module):
+    def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha, variant, pair):
+        super(GCNII, self).__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(nlayers):
+            self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant))
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(nfeat, nhidden))
+        self.fcs.append(nn.Linear(nhidden, nclass))
+        self.params1 = list(self.convs.parameters())
+        self.params2 = list(self.fcs.parameters())
+        self.act_fn = nn.ReLU()
+        self.dropout = dropout
+        self.alpha = alpha
+        self.lamda = lamda
+        
+        # add 
+        print( ' pair ===== 33333333' , pair) 
+        
+        if pair == 'PN':
+            self.norm = PairNorm('PN', 1)
+
+    def forward(self, x, adj):
+        _layers = []
+        
+       
+        
+        x = self.norm(x)
+        x = F.dropout(x, self.dropout, training=self.training)
+        
+        
+        
+        x = self.norm(x)
+
+        layer_inner = self.act_fn(self.fcs[0](x))
+        _layers.append(layer_inner)
+ 
+        for i,con in enumerate(self.convs):
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+            layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))
+        layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+        layer_inner = self.fcs[-1](layer_inner)
+        return F.log_softmax(layer_inner, dim=1)
+
+class GCNIIppi(nn.Module):
+    def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha,variant):
+        super(GCNIIppi, self).__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(nlayers):
+            self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant,residual=True))
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(nfeat, nhidden))
+        self.fcs.append(nn.Linear(nhidden, nclass))
+        self.act_fn = nn.ReLU()
+        self.sig = nn.Sigmoid()
+        self.dropout = dropout
+        self.alpha = alpha
+        self.lamda = lamda
+
+    def forward(self, x, adj):
+        _layers = []
+        x = F.dropout(x, self.dropout, training=self.training)
+        layer_inner = self.act_fn(self.fcs[0](x))
+        _layers.append(layer_inner)
+        for i,con in enumerate(self.convs):
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+            layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))
+        layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+        layer_inner = self.sig(self.fcs[-1](layer_inner))
+        return layer_inner
+
+class PairNorm(nn.Module):
+    def __init__(self, mode='PN-SI', scale=1):
+        """
+            mode:
+              'None' : No normalization 
+              'PN'   : Original version
+              'PN-SI'  : Scale-Individually version
+              'PN-SCS' : Scale-and-Center-Simultaneously version
+           
+            ('SCS'-mode is not in the paper but we found it works well in practice, 
+              especially for GCN and GAT.)
+            PairNorm is typically used after each graph convolution operation. 
+        """
+        assert mode in ['None', 'PN',  'PN-SI', 'PN-SCS']
+        super(PairNorm, self).__init__()
+        self.mode = mode
+        self.scale = scale
+
+        # Scale can be set based on origina data, and also the current feature lengths.
+        # We leave the experiments to future. A good pool we used for choosing scale:
+        # [0.1, 1, 10, 50, 100]
+                
+    def forward(self, x):
+        if self.mode == 'None':
+            return x
+        
+        col_mean = x.mean(dim=0)      
+        if self.mode == 'PN':
+            x = x - col_mean
+            rownorm_mean = (1e-6 + x.pow(2).sum(dim=1).mean()).sqrt() 
+            x = self.scale * x / rownorm_mean
+
+        if self.mode == 'PN-SI':
+            x = x - col_mean
+            rownorm_individual = (1e-6 + x.pow(2).sum(dim=1, keepdim=True)).sqrt()
+            x = self.scale * x / rownorm_individual
+
+        if self.mode == 'PN-SCS':
+            rownorm_individual = (1e-6 + x.pow(2).sum(dim=1, keepdim=True)).sqrt()
+            x = self.scale * x / rownorm_individual - col_mean
+
+        return x
+    
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
